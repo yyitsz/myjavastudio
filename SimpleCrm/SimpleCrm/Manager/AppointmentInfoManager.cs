20408933e95e77e6d1fbb7f4f820d5aba77ac0fa
yyitsz <yyitsz@163.com>
2014-11-09 22:43:19 +0800

    bugs..

--- SimpleCrm/SimpleCrm/Manager/AppointmentInfoManager.cs
+++ SimpleCrm/SimpleCrm/Manager/AppointmentInfoManager.cs
@@ -20,8 +20,13 @@ namespace SimpleCrm.Manager
 
         internal IEnumerable<AppointmentInfo> GetListByDateRange(String owner, DateTime startDate, DateTime endDate)
         {
-             IEnumerable<AppointmentInfo> list = Connection.GetList<AppointmentInfo>("Owner = @Owner and ( StartTime >= date(@StartDate) And StartTime < date(@EndDate) or EndTime >= date(@StartDate) And EndTime < date(@EndDate) ) ",
-                new {Owner = owner,  StartDate = startDate, EndDate = endDate.AddDays(1) });
+            IEnumerable<AppointmentInfo> list = Connection.Query<AppointmentInfo>(@"SELECT AppointmentInfoId, CustomerId, Category, StartTime, EndTime, Subject, Content, CategoryColor, TimerMarker, Owner, VersionNo, CreateTime, UpdatedBy, UpdateTime
+FROM AppointmentInfo
+union all
+SELECT FollowUpRecordId AppointmentInfoId, f.CustomerId, 'FollowUp' Category, NextFollowUpDate StartTime, date(NextFollowUpDate, '+1 day') EndTime, '预约再次跟进客户' || CustomerName Subject, Content Content, null CategoryColor, null TimerMarker, null Owner, f.VersionNo, f.CreateTime, f.UpdatedBy, f.UpdateTime
+FROM FollowUpRecord f INNER JOIN Customer c on f.CustomerId = c.CustomerId
+WHERE ( StartTime >= date(@StartDate) And StartTime < date(@EndDate) or EndTime >= date(@StartDate) And EndTime < date(@EndDate) ) ",
+                new {  StartDate = startDate, EndDate = endDate.AddDays(1) });
              list.MarkAsPersisted();
              return list;
         }
